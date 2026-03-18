c15bba1d3b3c8db07b3815af31ac30cefeb2cc67
kagemike <mikepopoekim@gmail.com>
2026-03-18 16:39:15 +0800

    3

---
+++ chose/chose/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace chose
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(637, 605);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(292, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開抽";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(122, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(299, 353);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(494, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(545, 574);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1051, 725);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ chose/chose/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chose
+{
+    public partial class Form1 : Form
+    {
+        // 1. 宣告一個陣列來存放你準備好的圖片資源
+        // 注意：這裡的 pic1, pic2, pic3 必須與你在「資源」中加入的圖片名稱一致
+        private Image[] prizeImages;
+
+        // 2. 宣告隨機數產生器
+        private readonly Random randomGenerator;
+
+        public Form1(Random randomGenerator)
+        {
+            this.randomGenerator = randomGenerator;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            // 在程式啟動時初始化圖片清單與亂數產生器
+            prizeImages = new Image[]
+            {
+                Properties.Resources.Bow,
+                Properties.Resources.Dual_Blades,
+                Properties.Resources.Great_Sword,
+                Properties.Resources.Gunlance,
+                Properties.Resources.hammer,
+                Properties.Resources.Heavy_Bowgun,
+                Properties.Resources.Hunting_Horn,
+                Properties.Resources.Insect_Glaive_1,
+                Properties.Resources.Lance,
+                Properties.Resources.Switch_Axe,
+                Properties.Resources.Sword___Shield,
+                Properties.Resources.Long_Sword,
+                Properties.Resources.Light_Bowgun,
+                Properties.Resources.Charge_Blade
+            };
+
+            randomGenerator = new Random();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 3. 產生一個介於 0 到 (圖片數量 - 1) 之間的隨機整數
+            int randomIndex = randomGenerator.Next(0, prizeImages.Length);
+
+            // 4. 將隨機選中的圖片，輸出 (賦值) 給 PictureBox 來顯示
+            pictureBox2.Image = prizeImages[randomIndex];
+
+        }
+    }
+}
